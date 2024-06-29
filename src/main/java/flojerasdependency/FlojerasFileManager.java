ca82a67c4538b08a08a41a502a296c31df6fc2df
Carlos Madrid <37978151+Nozferatu@users.noreply.github.com>
2024-06-29 15:06:25 +0200

    Nueva utilidad (version 0.6)

---
+++ src/main/java/flojerasdependency/FlojerasFileManager.java
@@ -0,0 +1,163 @@
+package flojerasdependency;
+
+//FLOJERAS FILE MANAGER V0.1
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Stack;
+
+/**
+ *
+ * @author Carlos Madrid
+ */
+public class FlojerasFileManager {
+    private File archivo;
+    private boolean accesible;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private Stack<String> queue;
+    private boolean debugMode;
+    
+    public FlojerasFileManager(String ruta){
+        this.archivo = new File(ruta);
+        if(!archivo.isFile()){
+            System.out.printf("LA RUTA NO ES VÁLIDA (%s): ARCHIVO NO VÁLIDO\n", ruta);
+            accesible = false;
+        }else{
+            accesible = true;
+        }
+        queue = new Stack();
+        debugMode = false;
+    }
+
+    public FlojerasFileManager(String archivo, boolean debugMode) {
+        this(archivo);
+        this.debugMode = debugMode;
+    }
+
+    /**
+     * Devuelve la ruta absoluta donde se encuentra el archivo
+     * @return String
+     */
+    public String rutaAbsoluta(){
+        return archivo.getAbsolutePath();
+    }
+    
+    /**
+     * Devuelve una cadena de texto del archivo. En caso de que no haya más por leer, devolverá null.
+     * @return String
+     */
+    public String leerLinea(){
+        if(accesible){
+            if(reader == null){
+                try {
+                    reader = new BufferedReader(new FileReader(archivo));
+                } catch (FileNotFoundException ex) {}
+            }
+            try {
+                return reader.readLine();
+            } catch (IOException ex) {
+                if(debugMode) {
+                    System.out.println("No se ha podido leer el archivo");
+                    ex.printStackTrace(System.out);
+                }
+                return null;
+            }
+        }else{
+            return null;
+        }
+    }
+    
+    /**
+     * Añade a una cola la cadena de texto que se proporcione. Si el parámetro de introducirNuevaLinea es verdadero, se añadirá un Enter. 
+     * Hasta que no se llame el método prepararCambios(), no se pasará al flujo de datos.
+     * @param linea String
+     * @param introducirNuevaLinea Boolean
+     */
+    public void escribirLinea(String linea, boolean introducirNuevaLinea){
+        if (introducirNuevaLinea){
+            queue.push(linea + '\n');
+        }else{
+            queue.push(linea);
+        }
+    }
+    
+    /**
+     * Añade todos los elementos de la cola al flujo de datos. Hasta que no se llame el método efectuarCambios(), no se realizará la escritura en el archivo.
+     */
+    public void prepararCambios(){
+        if(!queue.isEmpty()){
+            if(writer == null){
+                try {
+                    writer = new BufferedWriter(new FileWriter(this.archivo, true));
+                } catch (IOException ex) {}
+            }
+            try {
+                for(String item : queue)
+                    writer.write(item);
+            } catch (IOException ex) {
+                if(debugMode){
+                    System.out.println("No se ha podido escribir en el archivo");
+                    ex.printStackTrace(System.out);
+                }
+            }
+        }else{
+            System.out.println("No hay ningún cambio añadido a la cola.");
+        }
+    }
+    
+    /**
+     * Se realizan los cambios que hayan almacenados en el flujo de datos.
+     */
+    public void efectuarCambios(){
+        if(writer != null){
+            try {
+                writer.close();
+            } catch (IOException ex) {}
+        }else{
+            System.out.println("El flujo de datos no está abierto.");
+        }
+    }
+    
+    /**
+     * Elimina el último elemento añadido a la cola.
+     */
+    public void deshacerCambiosCola(){
+        queue.pop();
+    }
+    
+    /**
+     * Elimina todos los cambios pendientes en la cola.
+     */
+    public void limpiarCola(){
+        queue.clear();
+    }
+    
+    /**
+     * Elimina todos los cambios del flujo de datos.
+     */
+    public void limpiarFlujo(){
+        if(writer != null){
+            try {
+                writer.flush();
+            } catch (IOException ex) {}
+        }else{
+            System.out.println("El flujo de datos no está abierto.");
+        }
+    }
+    
+    /**
+     * Cierra el flujo de datos, tanto el de lectura como el de escritura. Es recomendable cuando ya no se siga leyendo/modificando el archivo.
+     */
+    public void cerrarFlujo() {
+        try {
+            if (reader != null) reader.close();
+            if (writer != null) writer.close();
+        } catch (IOException e) {}
+    }
+}
